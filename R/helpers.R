cdaeb5da5162104bec32f42f1ba77f07e66998f4
David Tan <david.tan.yk@gmail.com>
2020-12-07 10:43:23 +0800

    initial launch

---
+++ R/helpers.R
@@ -0,0 +1,11 @@
+#' Test whether the Independent Variable is a factor variable
+#'
+#' @param df Data Frame
+#' @param var Independent Variable (IV)
+#' @return Data Frame with IV as a factor data type
+#'
+test_factor <- function(df, var) {
+  if (!is.factor(df[, var])) {
+    df[, var] <- as.factor(df[, var])
+  }
+}
